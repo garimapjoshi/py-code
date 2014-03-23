6f5250f9c56af32bd4b6ee9e6934ea19494b886b
garimapjoshi <garimapjoshi@gmail.com>
2014-03-22 21:51:23 -0400

    Add WeekDays.py

---
+++ WeekDays.py
@@ -0,0 +1,6 @@
+days_of_week = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday']
+
+for week in range (1,5):
+	print "Week {0}".format(week)
+	for day in days_of_week:
+		print day
