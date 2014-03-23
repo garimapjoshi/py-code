0e76544fc9141c3350ba1cd20c864bc34b8a1a6c
garimapjoshi <garimapjoshi@gmail.com>
2014-03-22 22:23:11 -0400

    add 99BottlesofBeer.py

---
+++ 99BottlesofBeer.py
@@ -0,0 +1,9 @@
+for bottles in range (99, 1, -1):
+
+	print bottles
+	print "bottles of beer on the wall,"
+	print bottles
+	print "bottles of beer"
+	print "If one of those bottles should happen to fall. . ."
+	print bottles - 1
+	print "bottles of beer on the wall."
