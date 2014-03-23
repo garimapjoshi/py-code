18db1e78320c9e0a5d14abf91c6d766f58f6a13c
garimapjoshi <garimapjoshi@gmail.com>
2014-03-22 22:18:45 -0400

    add Conditional.py

---
+++ Conditional.py
@@ -0,0 +1,8 @@
+volun_recruit_goal=input("Enter the Volunteer Recruits Goal: ")
+curr_volun=input ("Enter the current number of volunteers: ")
+print "Volunteer Recruits Goal is ",volun_recruit_goal,"\n","The current number of volunteers are ",curr_volun
+if volun_recruit_goal >= curr_volun:
+	print "better luck next time!"
+else:
+	print"congratulations"  
+
